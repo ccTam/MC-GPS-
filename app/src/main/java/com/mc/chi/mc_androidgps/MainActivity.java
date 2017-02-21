938306706b0c14bc8acac88888ad43d283c401fc
Chi Tam <chi.tam.377@my.csun.edu>
2017-02-21 01:08:20 -0800

    Add location address

--- app/src/main/java/com/mc/chi/mc_androidgps/MainActivity.java
+++ app/src/main/java/com/mc/chi/mc_androidgps/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,12 +17,16 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private boolean iniValue = true;
     private LocationManager locManager;
     private LocationListener locListener;
     private double minX, minY, minZ, maxX, maxY, maxZ;
-    private TextView[] GPS_TextView = new TextView[3];
+    private TextView[] GPS_TextView = new TextView[4];
     String s = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         GPS_TextView[0] = (TextView) findViewById(R.id.locLonValue);
         GPS_TextView[1] = (TextView) findViewById(R.id.locLatValue);
         GPS_TextView[2] = (TextView) findViewById(R.id.locAltValue);
+        GPS_TextView[3] = (TextView) findViewById(R.id.locName);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         }
         locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -42,6 +49,28 @@ public class MainActivity extends AppCompatActivity {
                         double locLat = loc.getLatitude();
                         double locAlt = loc.getAltitude();
                         setPeak(locLon, locLat, locAlt);
+                        Geocoder geoCoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                        StringBuilder builder = new StringBuilder();
+                        List<Address> address = null;
+                        try {
+                            if (geoCoder.isPresent()){
+                                address = geoCoder.getFromLocation(locLat, locLon, 3);
+                                int maxLines = address.get(0).getMaxAddressLineIndex();
+                                for (int i=0; i<maxLines; i++) {
+                                    String addressStr = address.get(0).getAddressLine(i);
+                                    builder.append(addressStr);
+                                    builder.append(" ");
+                                }
+                                String finalAddress = builder.toString(); //This is the complete address.
+                                GPS_TextView[3].setText("\n" + finalAddress); //display final address.
+                            }else{
+                                GPS_TextView[3].setText("\n!!GeoCoder not found!!"); //display final address.
+                            }
+                        } catch (IOException e) {
+                            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT);
+                        } catch (NullPointerException e) {
+                            Toast.makeText(getApplicationContext(),  e.toString(), Toast.LENGTH_SHORT);
+                        }
                     }
                 }
 
